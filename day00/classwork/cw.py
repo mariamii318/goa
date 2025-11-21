047a9ce9bf608119b7406b4fe1a8f03e16cd9282
mariamii318 <mariamkobaidze2006@icloud.com>
2025-11-21 20:35:16 +0400

    1

---
+++ day00/classwork/cw.py
@@ -0,0 +1,57 @@
+# print('hello world')
+# #nebismieri ram rac brchkalebshi aris mocemuli ewodeba stringi
+# print(5)#integer integeri ewodeba mtel ricxvs iseni ar arian motavsebulebi brchkalebshi
+# print("5")#stringi
+from turtle import *
+width(7)
+color("red")
+
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(70)
+
+
+
+color("blue")
+left(90)
+forward(100)
+right(90)
+forward(60)
+right(90)
+forward(100)
+
+
+penup()
+goto(200,200)
+pendown()
+
+color("purple")
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+
+
+
+color("red")
+left(30)
+forward(40)
+left(90)
+color("yellow")
+begin_fill()
+forward(50)
+right(90)
+forward(50)
+right(90)
+forward(50)
+end_fill()
+exitonclick()
+
